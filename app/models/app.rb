ed584146e937d14c5c4d37becce2dc5611faf4c7
Samuel Clark <sam.clark@me.com>
2015-02-23 16:15:47 -0800

    Added s3 protocol to change http into https

--- app/models/app.rb
+++ app/models/app.rb
@@ -2,4 +2,9 @@ class App < ActiveRecord::Base
 	belongs_to :user
 	 has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" } #, :default_url => "http://www.grammarly.com/blog/wp-content/uploads/2015/01/Silhouette-question-mark.jpeg"
 	 validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
+
+	 def s3_credentials
+    {:s3_protoccol => 'https'}
+  end
 end
