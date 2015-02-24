99557ede05dc8d9fc2dd8e70e1c25f81543ded0b
Samuel Clark <sam.clark@me.com>
2015-02-23 16:20:25 -0800

    Changed the heroku amazon thing

--- app/models/app.rb
+++ app/models/app.rb
@@ -3,8 +3,4 @@ class App < ActiveRecord::Base
 	 has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" } #, :default_url => "http://www.grammarly.com/blog/wp-content/uploads/2015/01/Silhouette-question-mark.jpeg"
 	 validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
-
-	 def s3_credentials
-    {:s3_protoccol => 'https'}
-  end
 end
