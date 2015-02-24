80f19b211f970eae83cb0b8822921a0a2005d699
Samuel Clark <sam.clark@me.com>
2015-02-23 16:23:51 -0800

    bug testing heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,10 +83,11 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
+    :s3_protocol => 'https'
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    :s3_protocol => 'https'
+    :s3_permissions => :private
   }
 }
 
