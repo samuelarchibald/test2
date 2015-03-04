3ca66c5261de517f1de2c813b2ca7ba8841c9f2f
Samuel Clark <sam.clark@me.com>
2015-03-03 16:18:23 -0800

    Added activeadmin

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :accounts, :only => [:index]
 
   resources :apps
